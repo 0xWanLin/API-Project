145a296f4895fbf10d7325c111c06c1b69a67639
Wan Lin <75497384+0xWanLin@users.noreply.github.com>
2020-12-11 01:36:35 +0800

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,3 @@
+# just an empty file 
+# "__init__" is a reseved method in python classes. It is called as a constructor in object oriented terminology. 
+# This method is called when an object is created from a class and it allows the class to initialize the attributes of the class.
