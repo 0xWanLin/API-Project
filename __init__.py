4a455ca1fa5601dc59ffe0c9bf35b8d7ecf2ea11
Wan Lin <75497384+0xWanLin@users.noreply.github.com>
2020-12-16 14:50:10 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,3 +0,0 @@
-# just an empty file 
-# "__init__" is a reseved method in python classes, called as a constructor in object oriented terminology
-# this method is called when an object is created from a class and it allows the class to initialize the attributes of the class
